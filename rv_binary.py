ea9e3aafad2e3b264fd88dca03044958bf9e5460
Devin Chu <dchu@astro.ucla.edu>
2016-11-15 09:54:43 -0800

    starting to develop the orbital model envelope stage

--- rv_binary.py
+++ rv_binary.py
@@ -1,12 +1,47 @@
 ##program explore binary stars with rv data - D.Chu, 2016-11-09
 
 import numpy as np
+import pylab
 import matplotlib.pyplot as plt
 import asciidata
 import efit5_util_final
 from astropy.stats import LombScargle
 import astropy.units as u
 
+def extract_chains(mnestfile,star_num=0):
+    ##extract a model from the multinest run, usually file is efit_.txt
+    ##a lot of this code came from efit5_util_final.plot_param_hist
+    ##this only focuses on when BH parameters are not fixed
+    ##also assumes no extended mass
+
+    ##get information from the chains file
+	##came from around line 142 of efit5_util_final.plot_param_hist
+    inFile = np.genfromtxt(mnestfile)
+    ##weights are the first column of chains file
+    weights = inFile[:,0]
+    
+    ##get orbital parameter values from chains file
+    mass = inFile[:,2]
+    xo = -inFile[:,3]
+    yo = inFile[:,4]
+    Vx = -inFile[:,5]
+    Vy = inFile[:,6]
+    Vz = inFile[:,7]
+    D = inFile[:,8]
+    Omega = inFile[:,9+star_num*6]
+    omega = inFile[:,10+star_num*6]
+    i = inFile[:,11+star_num*6]
+    P = inFile[:,12+star_num*6]
+    To = inFile[:,13+star_num*6]
+    e = inFile[:,14+star_num*6]
+    T_next = To + P
+    logLikes = inFile[:,1]
+    where_max = np.argmin(logLikes)
+    
+	##make histogram of the chains values, with weights
+	
+	
+    
 def rv_resid_file(rv_file,model_file,star):
     rv_table = asciidata.open(rv_file)
     #read in RV data
