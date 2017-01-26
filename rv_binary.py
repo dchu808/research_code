a6e682320e88036622789775efee1f4d29298b23
Devin Chu <dchu@astro.ucla.edu>
2017-01-25 16:52:26 -0800

    fixed another typo

--- rv_binary.py
+++ rv_binary.py
@@ -208,6 +208,7 @@ def plot_env(freqarray,median,plus_env,minus_env):
     plt.ylabel('Power')
     # plt.ylim(0,1.5)
     #plt.xlim(0,30)
+    plt.xlim(1,2400)    
     plt.show()
 
     # plt.semilogx(1/frequency, median - minus, color ='black')
